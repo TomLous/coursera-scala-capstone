28a198138d3198877cf8b120c037ad4899c399fc
Tom Lous <tomlous@gmail.com>
2017-04-03 21:48:50 +0200

    analyzer ignores log4j.properties => Fixed in code

--- src/main/scala/observatory/utils/SparkJob.scala
+++ src/main/scala/observatory/utils/SparkJob.scala
@@ -1,6 +1,11 @@
 package observatory.utils
 
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
 import org.apache.spark.sql.SparkSession
+
+import scala.util.Try
 
 /**
   * Created by Tom Lous on 24/03/17.
@@ -8,10 +13,14 @@ import org.apache.spark.sql.SparkSession
   */
 trait SparkJob {
 
+  Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+
   implicit val spark:SparkSession = SparkSession
     .builder()
     .master("local[6]")
     .appName(this.getClass.getSimpleName)
     .getOrCreate()
+
+
 
 }
