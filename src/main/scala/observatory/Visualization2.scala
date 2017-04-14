d00b632a34b2e467a4ecffed33849f2dd2ccc653
Tom Lous <tomlous@gmail.com>
2017-04-14 11:05:11 +0200

    Submitted again

--- src/main/scala/observatory/Visualization2.scala
+++ src/main/scala/observatory/Visualization2.scala
@@ -62,61 +62,23 @@ object Visualization2 {
     val imageWidth = 256
     val imageHeight = 256
 
-    val pixels = (0 until imageWidth * imageHeight) // for each pixel
-      .par.map(pos => {
+    val pixels = tileLocations(x, y, zoom, imageWidth, imageHeight).par.map{
+      case (pos,pixelLocation) => {
 
-      val relativeXPosInTile = (pos % imageWidth).toDouble / imageWidth
-      val relativeYPosInTile = (pos / imageHeight).toDouble / imageHeight
+        val d00 = grid(ceil(pixelLocation.lat).toInt, floor(pixelLocation.lon).toInt) // nw
+        val d01 = grid(floor(pixelLocation.lat).toInt, floor(pixelLocation.lon).toInt) // sw
+        val d10 = grid(ceil(pixelLocation.lat).toInt, ceil(pixelLocation.lon).toInt) // ne
+        val d11 = grid(floor(pixelLocation.lat).toInt, ceil(pixelLocation.lon).toInt) // se
 
-      val absoluteXPos = relativeXPosInTile + x // column of image as fraction with offset x
-      val absoluteYPos = relativeYPosInTile + y // row of image as fraction with offset y
+        val xFraction = pixelLocation.lon - floor(pixelLocation.lon)
+        val yFraction = ceil(pixelLocation.lat) - pixelLocation.lat
 
-      val pixelLocation = Tile(absoluteXPos, absoluteYPos, zoom).location
 
-      val d00 = grid(ceil(pixelLocation.lat).toInt, floor(pixelLocation.lon).toInt) // nw
-      val d01 = grid(floor(pixelLocation.lat).toInt, floor(pixelLocation.lon).toInt) // sw
-      val d10 = grid(ceil(pixelLocation.lat).toInt, ceil(pixelLocation.lon).toInt) // ne
-      val d11 = grid(floor(pixelLocation.lat).toInt, ceil(pixelLocation.lon).toInt) // se
-
-      val xFraction = pixelLocation.lon - floor(pixelLocation.lon)
-      val yFraction = ceil(pixelLocation.lat) - pixelLocation.lat
-
-//      val interpolatedValue = bilinearInterpolation(x=relativeXPosInTile, y=relativeYPosInTile, d00=d00, d01=d01, d10=d10, d11=d11)
-
-      println(s"x: $x, y: $y, zoom: $zoom, location: $pixelLocation, relXPos: $xFraction, relYPos: $yFraction, d00: $d00, d01: $d01, d10: $d10, d11: $d11")
-
-// -123 - -123.5
-
-//      // note that loc is the Location of the pixel!
-//      val d00 = grid(scala.math.ceil(loc.lat).toInt, scala.math.floor(loc.lon).toInt) // nw
-//      val d01 = grid(scala.math.floor(loc.lat).toInt, scala.math.floor(loc.lon).toInt) // sw
-//      val d10 = grid(scala.math.ceil(loc.lat).toInt, scala.math.ceil(loc.lon).toInt) // ne
-//      val d11 = grid(scala.math.floor(loc.lat).toInt, scala.math.ceil(loc.lon).toInt) // se
-
-//      val gridValues: Map[(Int, Int), Double] = {
-//        for {
-//          tileX <- 0 to 1
-//          tileY <- 0 to 1
-//        } yield (tileY, tileX) -> {
-//          val tileLocation = Tile(tileX + x, tileY + y, zoom).location
-//          val lon = tileLocation.lonInt(tileX == 1) min 179 max -180
-//          val lat = tileLocation.latInt(tileY == 1) min 90 max -89
-////          if(lon < -180 || lon > 179 || lat < -89 || lon > 90){
-////          if(zoom != 0){
-//          if(x >= 130 && x < 150 && y== 0){
-//            println(s"x: $x, y: $y, zoom: $zoom, location: $tileLocation, relXPos: $relXPos, relYPos: $relYPos, tileX: $tileX, tileY: $tileY, lat: $lat, lon: $lon")
-//          }
-//          grid(lat, lon)
-//        }
-//      }.toMap
-
-      pos -> interpolateColor(
-        colors,
-//        bilinearInterpolation(relXPos, relYPos, gridValues((0,0)), gridValues((0,1)), gridValues((1,0)), gridValues((1,1)))
-//        bilinearInterpolation(relYPos, relXPos, d00=d00, d01=d01, d10=d10, d11=d11)
-        bilinearInterpolation(x=xFraction, y=yFraction, d00=d00, d01=d01, d10=d10, d11=d11)
-      ).pixel(127)
-    })
+        pos -> interpolateColor(
+          colors,
+          bilinearInterpolation(x=xFraction, y=yFraction, d00=d00, d01=d01, d10=d10, d11=d11)
+        ).pixel(127)
+      }}
       .seq
       .sortBy(_._1)
       .map(_._2)
--- src/test/scala/observatory/Visualization2Test.scala
+++ src/test/scala/observatory/Visualization2Test.scala
@@ -35,4 +35,13 @@ class Visualization2Test extends FunSuite with Checkers {
     assert(method1 === method2)
   }
 
+  test("bilinearInterpolation"){
+
+    assert(Visualization2.bilinearInterpolation(0.5, 0.5, 10, 20, 30, 40) === 25.0)
+    assert(Visualization2.bilinearInterpolation(0.1, 0.5, 10, 20, 30, 40) === 17.0)
+    assert(Visualization2.bilinearInterpolation(0.5, 0.1, 10, 20, 30, 40) === 21.0)
+    assert(Visualization2.bilinearInterpolation(0.9, 0.1, 10, 20, 30, 40) === 29.0)
+    assert(Visualization2.bilinearInterpolation(1.0, 0.0, 10, 20, 30, 40) === 30.0)
+  }
+
 }
